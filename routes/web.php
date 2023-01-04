1a041e1ac41b234101d794b3cac98ea684d175c8
Erfan Ebrahimi <persionhost@gmail.com>
2023-01-04 19:13:33 +0330

    add login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/home');
+
+Auth::routes();
+Route::post('register' , [App\Http\Controllers\Auth\RegisterController::class , 'register' ])->middleware('auth');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
